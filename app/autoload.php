e05e76d7ec5e1ba2c11e339d60e54cd8212af9ce
John Kary <john@johnkary.net>
2012-09-24 23:52:49 -0500

    Check for installed dependencies before executing
    Thanks Packagist: https://github.com/composer/packagist/blob/d59dc2b0d3074fba52eefceeabf7366ad50250ff/app/autoload.php

---
+++ app/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
+
+    $message = <<< EOF
+<p>You must set up the project dependencies by running the following commands:</p>
+<pre>
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+</pre>
+
+EOF;
+
+    if (PHP_SAPI === 'cli') {
+        $message = strip_tags($message);
+    }
+
+    die($message);
+}
+
+return $loader;
